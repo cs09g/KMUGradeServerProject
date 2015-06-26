dc9527579537a5558b0fc24216af052355df6abe
astroluj <astrolujx@naver.com>
2015-06-26 13:55:17 +0900

    paging auto sort

--- pydev/GradeServer/GradeServer/GradeServer/utils/utilPaging.py
+++ pydev/GradeServer/GradeServer/GradeServer/utils/utilPaging.py
@@ -15,11 +15,10 @@ def get_page_pointed(pageNum, count, BLOCK = OtherResources().const.BLOCK, LIST 
     
     #show Page
     allPage = int(math.ceil(count / float(LIST)))
-    #block = pageNum / BLOCK if pageNum < BLOCK else pageNum - BLOCK
-    #startPage = block + 1 
-    #endPage = block + BLOCK 
-    startPage = 1 if pageNum - 3 < 1 else (allPage - BLOCK  +1 if pageNum + 3 > allPage else pageNum - 3)
-    endPage = pageNum if pageNum == allPage else (BLOCK if startPage == 1 else pageNum + 2)
+    extraPage = BLOCK / 2
+    startPage = 1 if pageNum <= extraPage\
+                     or allPage <= BLOCK else pageNum - extraPage
+    endPage = pageNum if pageNum == allPage else (BLOCK if startPage == 1 else pageNum + extraPage)
     #Minimum Page
     if endPage > allPage:
         endPage = allPage
